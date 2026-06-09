0cc4ec4f8f85289b0d4720a315f6a59e85525b24
Bruno <b-d.dupuis@laposte.net>
2026-06-09 21:58:24 +0200

    test-identity

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import homeData from '../content/home.json'
+</script>
+
+<template>
+  <section class="hero">
+    <h1>{{ homeData.title }}</h1>
+
+    <p>{{ homeData.subtitle }}</p>
+
+    <img :src="homeData.heroImage" />
+
+    <a :href="'tel:' + homeData.phone">
+      {{ homeData.phone }}
+    </a>
+  </section>
+</template>
